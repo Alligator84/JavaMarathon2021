b844201ded498d4e4274407ad3aa1b73aa29e089
Oleg Yudin <olekat84@gmaill.com>
2021-02-25 15:06:57 +0600

    Add tasks day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner strNumber = new Scanner(System.in);
+        double delimoe = strNumber.nextDouble();
+        double delitel = strNumber.nextDouble();
 
+        while (true) {
+            if (delitel == 0) {
+                break;
+            }
+            System.out.println(delimoe / delitel);
+            delimoe = strNumber.nextDouble();
+            delitel = strNumber.nextDouble();
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,19 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner strNumber = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            double delimoe = strNumber.nextDouble();
+            double delitel = strNumber.nextDouble();
+            if (delitel == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            } else {
+                System.out.println(delimoe / delitel);
+            }
+        }
     }
 }
