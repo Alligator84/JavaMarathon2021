5d38c776a0a1b7ddbcecce2048e235ba92458147
Oleg Yudin <olekat84@gmaill.com>
2021-02-27 10:41:22 +0500

    Add tasks day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,72 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car carOne = new Car();
+        carOne.setColor("red");
+        carOne.setYear(2020);
+        carOne.setModel("vaz 2101");
+
+        System.out.println(carOne.getModel());
+        System.out.println(carOne.getYear());
+        System.out.println(carOne.getColor());
 
     }
+}
+
+class Car {
+    private int year;
+    private String color;
+    private String model;
+
+    public void setYear(int carYear) {
+        if (carYear <=0 ) {
+            System.out.println("Год выпуска автомобиля целочисленное число строго больше нуля");
+        } else {
+            year = carYear;
+        }
+    }
+
+    public int getYear() {
+      if (year == 0) {
+          System.out.println("Не установлено значение поля объекта Year");
+          return 0;
+      } else {
+          return year;
+      }
+    }
+
+    public void setColor(String carColor) {
+        if (carColor.isEmpty()) {
+            System.out.println("Цвет автомобиля не задан");
+        } else {
+            color = carColor;
+        }
+    }
+
+    public String getColor() {
+        if (color.isEmpty()) {
+            System.out.println("Не установлено значение поля объекта Color");
+            return null;
+        } else {
+            return color;
+        }
+    }
+
+    public void setModel(String carModel) {
+        if (carModel.isEmpty()) {
+            System.out.println("Модель автомобиля не задана");
+        } else {
+            model = carModel;
+        }
+    }
+
+    public String getModel() {
+        if (model.isEmpty()) {
+            System.out.println("Не установлено значение поля объекта Model");
+            return null;
+        } else {
+            return model;
+        }
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,33 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motoOne = new Motorbike(2020, "Black", "Kawasaki 1010");
+        System.out.println(motoOne.getYear());
+        System.out.println(motoOne.getModel());
+        System.out.println(motoOne.getColor());
+    }
+}
 
+class Motorbike {
+    private int year;
+    private String color;
+    private String model;
+
+    public Motorbike(int motoYear, String motoColor, String motoModel) {
+        this.year = motoYear;
+        this.color = motoColor;
+        this.model = motoModel;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
     }
 }
