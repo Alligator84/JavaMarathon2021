c93b48c1cc5809cc86b6b75d3d903793cce1a333
Oleg Yudin <alligator84@Olegs-MacBook-Pro.local>
2021-03-15 12:03:49 +0600

    Add tasks day15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,33 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File inputFile = new File("src/main/resources/shoes.csv");
+        File outputFile = new File("src/main/resources/missing_shoes.txt");
+        outShoesFile(inputFile, outputFile);
+    }
+
+    public static void outShoesFile(File inputFile, File outputFile) {
+        try {
+            Scanner input = new Scanner(inputFile);
+            PrintWriter output = new PrintWriter(outputFile);
+            while (input.hasNextLine()) {
+                String inputStringFile = input.nextLine();
+                String[] strArray = inputStringFile.split(";");
+                if (strArray[2].equals("0")) {
+                    output.println(inputStringFile);
+                }
+            }
+            input.close();
+            output.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Input or output file not found");
+        }
 
     }
 }
